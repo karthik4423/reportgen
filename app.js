b1e535f140c9f611409ff430ce831f0dac1d2af4
Karthik Raj <karthikraj4423@gmail.com>
2021-08-26 19:46:11 +0530

    app changes

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-app.listen(80, () => {
+app.listen(3110, () => {
     console.log("App is listening at port 80");
 });
 
+app.get("/",function(req,res){
+    res.send("index");
+})
 app.get("/reportId=MQEC8253_472104521", function (req, res) {
     res.download("public/ftth.pdf");
 });
